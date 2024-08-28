bb186ef7b0b9cdbc7f575d925a1021d6111e6157
Yuchen <gengyuchen@sina.com>
2024-08-28 19:50:56 +0800

    1

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -1,5 +1,14 @@
-#add cl to first of path
-cldir=$(dirname $(which cl))
-echo $cldir
-export PATH=cldir:$PATH
-echo $PATH
+##add cl to first of path
+#cldir=$(dirname $(which cl))
+#echo $cldir
+#export PATH=cldir:$PATH
+#echo $PATH
+
+
+pacman -S make
+pacman -S diffutils
+
+#cd ffmpeg
+#./configure --target-os=win64 --arch=x86_64 --toolchain=msvc -prefix=../build
+#make -j4
+
