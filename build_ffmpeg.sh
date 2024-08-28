fa19fa284cc6c8e7dac0cacfd8da978fc5c522f5
Yuchen <gengyuchen@sina.com>
2024-08-28 19:41:33 +0800

    1

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -1,6 +1,5 @@
-##add cl to first of path
-#cldir=$(dirname $(which cl))
-#echo $cldir
-#export PATH=cldir:$PATH
-#echo $PATH
-which cl
+#add cl to first of path
+cldir=$(dirname $(which cl))
+echo $cldir
+export PATH=cldir:$PATH
+echo $PATH
