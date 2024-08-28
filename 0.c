987a2a3b0884cbd875e3a0d6ca0afcaf5db630f0
Yuchen <gengyuchen@sina.com>
2024-08-28 17:20:03 +0800

    1

---
+++ 0.c
@@ -0,0 +1,10 @@
+
+#include <libavutil/log.h>
+//const char* av_default_item_name(void* ptr)
+int main() {
+	void* a = 0;
+	//swr_get_class();
+	av_default_item_name(a);
+	//AVFormatContext* pFormatContext = avformat_alloc_context();
+	return 0;
+}
